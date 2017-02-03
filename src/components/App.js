8bf217c3fefd4eb03b5c8df3bbe6f592f112956d
Michael Schennum <mikeschenphoto@gmail.com>
2017-02-03 10:18:47 -0700

    adds basic files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="schennum-blog">
+				<h1>Blog</h1>
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+import './css/style.css';
+import App from './components/App';
+
+const Root = () => {
+	return (
+		<BrowserRouter>
+		<div>
+			<Match exactly pattern="/" component={App} />
+		</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.querySelector('#main'));
