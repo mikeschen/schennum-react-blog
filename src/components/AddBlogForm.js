aa90cdfe0d26e3f86102b354f33a5ef55f0f19e5
mikeschen <mikeschenphoto@gmail.com>
2017-07-12 14:41:34 -0700

    fixes css

--- src/components/AddBlogForm.js
+++ src/components/AddBlogForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Input, Card, Row, Col } from 'react-materialize';
+import { Button, Card, Input, Row, Col } from 'react-materialize';
 
 class AddBlogForm extends React.Component {
 
@@ -21,12 +21,13 @@ class AddBlogForm extends React.Component {
 		return (
 
 			<form ref={(input) => this.blogForm = input} className="blog-edit" onSubmit={(e) => this.createBlog(e)}>
-				<Row>
-				<Input s={12} ref={(input) => this.title = input}type="text" label="Blog Title" />
-				<Input s={12} ref={(input) => this.desc = input}type="text" label="Blog Description"></Input>
-				<Input s={12} ref={(input) => this.image = input}type="text" label="Blog Image" />
+			<row>
+				<input ref={(input) => this.title = input}type="text" placeholder="Blog Title" />
+				<input ref={(input) => this.desc = input}type="text" placeholder="Blog Description" />
+				<input ref={(input) => this.image = input}type="text" placeholder="Blog Image" />
+
 				<Button waves='light' type="submit">Add Blog</Button>
-				</Row>
+			</row>
 			</form>
 		)
 	}
