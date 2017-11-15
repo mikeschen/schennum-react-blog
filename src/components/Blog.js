ab0f3524f1b9efecf923f87dae7a2e6556a9ce93
Michael Schennum <mikeschenphoto@gmail.com>
2017-11-15 10:07:18 -0700

    add setstate to likes button

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,25 +2,36 @@ import React from 'react';
 import { Card, CardTitle } from 'react-materialize';
 
 class Blog extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={ count: 1}
+    }
+
+    onclick(type){
+        this.setState(prevState => {
+            return {count: prevState.count + 1}
+        });
+    }
+
 	render() {
 
-		const  { details } = this.props;
-    let imageStyle = {
-			width: '400px'
-    };
-		return (
+	const  { details } = this.props;
+
+	return (
 			<div className="row">
-			<div className="col s6">
-			<Card className='medium'
-				header={<CardTitle image={this.props.details.image}>
-					{details.title}
-					</CardTitle>}>
-				{details.dateBlog} - {details.desc} 
-			</Card>
-				<div className="control-buttons">
-					<button className="likes">&hearts;</button>
+				<div className="col s6">
+					<Card className='medium'
+						header={
+							<CardTitle image={details.image}>
+								{details.title}
+							</CardTitle>}>
+						{details.dateBlog} - {details.desc}
+						<div style={{marginTop: '15px'}}>
+							<button onClick={this.onclick.bind(this, 'add')}>&hearts; {this.state.count}</button>
+						</div>
+					</Card>
 				</div>
-			</div>
 			</div>
 		)
 	}
